aac55e9c2a91e26b23ed9f285ed9a6c40f329097
maximefromont <maxime.fromont0@gmail.com>
2024-01-11 15:28:58 +0100

    Refactored folders organisation and added button easy script template

---
+++ Assets/Button-1_prefab/SpawnCubeOnButtonPress.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnCubeOnButtonPress : MonoBehaviour
+{
+
+    private ButtonBehavior buttonBehavior;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //USE THIS TO KNOW WHEN THE BUTTON IS PRESSED
+        // Get the ButtonBehavior component attached to the GameObject
+        buttonBehavior = GetComponent<ButtonBehavior>();
+
+        // Subscribe to the button press event and define the behavior
+        buttonBehavior.OnButtonPress += HandleButtonPress;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void HandleButtonPress()
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        
+        //Make sure to get the local position of the button
+        Vector3 button_position = buttonBehavior.transform.position;
+
+        cube.transform.position = button_position + new Vector3(-1.0f, 2.0f, 0.0f);
+
+        print("Button pressed! Cube created at " + cube.transform.position.ToString());
+
+        //Add physic to the cube
+        Rigidbody cube_rigidbody = cube.AddComponent<Rigidbody>();
+
+    }
+}
